7978864349ba928ca7c04d216879785fd5269dc7
kryspinm97 <maneskryspin69@gmail.com>
2022-10-08 10:32:18 +0000

    Created objects array for game Rules

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+var rules = { 
+
+  Rock : {name: "Rock", defeats: ["scissors","lizard"]},
+  Paper : {name: "Paper", defeats: ["rock","spock"]},
+  Scissors : {name: "Scissors", defeats: ["paper", "lizard"]},
+  Lizard : {name: "Lizard", defeats: ["paper", "spock"]},
+  Spock : {name: "Spock", defeats: ["scissors", "rock"]}
+
+};
