42458c2fb0bb79dfdaa3851ccca39e47e08cd960
kryspinm97 <maneskryspin69@gmail.com>
2022-10-08 16:43:15 +0000

    Created game function and game rules

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,55 @@
-var rules = { 
+let playerScore = document.getElementById('playerScore');
+let computerScore = document.getElementById('computerScore');
 
-  Rock : {name: "Rock", defeats: ["scissors","lizard"]},
-  Paper : {name: "Paper", defeats: ["rock","spock"]},
-  Scissors : {name: "Scissors", defeats: ["paper", "lizard"]},
-  Lizard : {name: "Lizard", defeats: ["paper", "spock"]},
-  Spock : {name: "Spock", defeats: ["scissors", "rock"]}
+let picks = [
+  {
+    "id": "Rock"
+  },
+  {
+    "id": "Paper"
+  },
+  {
+    "id": "Scissors"
+  },
+  {
+    "id": "Lizard"
+  },
+  {
+    "id": "Spock"
+  } 
+];
 
+let score = 0;
+
+let gameRules = {
+
+  Rock:     ["Lizard", "Scissor"],
+  Paper:    ["Rock", "Spock"],
+  Scissors: ["Paper", "Lizard"],
+  Lizard:   ["Spock", "Paper"],
+  Spock:    ["Scissors", "Rock"],
+};
+
+let btn = document.getElementById("Rock");
+btn.addEventListener('click', event => {
+  gameResult(0, 3);
+});
+
+function gameResult (player1, Comp) {
+
+  const playerPick = picks[player1].id;
+
+  const compPick = picks[Comp].id;
+
+  const playerRulePick = gameRules[playerPick];
+
+  if(playerPick === compPick){
+  console.log("Draw");
+  }else if(playerRulePick.includes(compPick)){
+    console.log("Player wins");
+    playerScore.innerHTML + 1;
+    console.log(score)
+  }else{
+    console.log("Computer wins")
+  }
 };
